64afe4a6ceb90442c7a7d1b8e7af6db155e3b986
NicoloMartiradonna <116425882+NicoloMartiradonna@users.noreply.github.com>
2022-12-17 09:57:54 +0100

    Add files via upload

---
+++ Compito del 17-12-2022/static/js/index.js
@@ -0,0 +1,17 @@
+let w=400
+let h=400
+function setup(){
+    createCanvas(w,h);
+    background(0, 255, 255);
+}
+
+function draw(){
+   let r=random()
+   if(r>w){
+    line(400, 400, 300, 300)
+   }
+   else
+   {
+    line(400, 400, 300, 300)
+   }
+}
